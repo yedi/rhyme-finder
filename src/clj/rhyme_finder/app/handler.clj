b962b1977c89115f424451aa83e034eb39304faf
yedi <yedispaghetti@gmail.com>
2013-12-28 22:03:34 -0500

    add ability to store analsysis via the web UI

--- src/clj/rhyme_finder/app/handler.clj
+++ src/clj/rhyme_finder/app/handler.clj
@@ -1,5 +1,6 @@
 (ns rhyme-finder.app.handler
   (:use compojure.core)
+  (:use ring.middleware.edn)
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
             [compojure.response :as response]
@@ -8,13 +9,35 @@
             [rhyme-finder.core :as rhyme]
             [rhyme-finder.app.db :as db]))
 
+(defn generate-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/edn"}
+   :body (pr-str data)})
+
+(defn analyze!
+  ([filename]
+     (analyze! filename (slurp filename)))
+  ([title txt]
+     (let [poem (rhyme/format-as-poem txt)
+           rs (rhyme/rhyme-streams poem 2 6 36 2)]
+       (db/add-poem-analysis! title txt (pr-str rs))
+       rs)))
+
+(defn new-analysis [req]
+  (let [title (-> req :params :title)
+        txt (-> req :params :text)
+        rs (analyze! title txt)]
+    (generate-response (rhyme/rhyme-combos rs))))
+
 (defroutes app-routes
   (GET "/" [] (resp/file-response "src/clj/rhyme_finder/app/client.html"))
+  (POST "/analyze" req (new-analysis req))
   (route/resources "/")
   (route/not-found "Page not found"))
 
 (def app
   (-> app-routes
+      wrap-edn-params
       handler/site))
 
 (defn start-server [port]
@@ -24,11 +47,3 @@
   (let [port (Integer. (or (first args) "3000"))]
     (start-server port)))
 
-(defn analyze
-  ([filename]
-     (analyze filename (slurp filename)))
-  ([title txt]
-     (let [poem (rhyme/format-as-poem txt)
-           rs (rhyme/rhyme-streams poem 2 6 36 2)]
-       (db/add-poem-analysis! title txt (pr-str rs))
-       rs)))
--- src/cljs/rhyme_finder/rhymer.cljs
+++ src/cljs/rhyme_finder/rhymer.cljs
@@ -2,11 +2,10 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]]
                    [dommy.macros :refer [node sel sel1]])
   (:require [goog.events :as events]
-            [cljs.core.async :refer [<! >! put! chan]]
+            [cljs.core.async :refer [<! >! put! take! chan]]
             [cljs.reader :refer [read-string]]
-            [dommy.core :as dom])
-  (:import [goog.net Jsonp]
-           [goog Uri]))
+            [dommy.core :as dom]
+            [ajax.core :refer [GET POST]]))
 
 (enable-console-print!)
 
@@ -19,10 +18,26 @@
            (cond prevent? (.preventDefault e))))
        out)))
 
+(defn post! [uri data]
+  (let [out (chan)]
+    (POST uri {:params data
+               :handler (fn [resp] (put! out resp))})
+    out))
+
+(defn handle-new-analysis []
+  (print "handling new analysis")
+  (let [out (chan)
+        poem-title (.-value (sel1 :#poem-title))
+        poem-txt (.-value (sel1 :#poem-text))
+        req (post! "/analyze" {:title poem-title :text poem-txt})]
+    (take! req (fn [resp] (put! out resp)))
+    out))
+
 (defn init []
   (let [new-analyses (listen (sel1 :#add-poem-form) "submit" true)]
     (go (while true
           (<! new-analyses)
-          (print "got new analysis")))))
+          (print (<! (handle-new-analysis)))
+          (print "analysis added to the database")))))
 
 (init)
