2c55f2773a3c9c8c20e41d70c12816b1a81522a2
yedi <yedispaghetti@gmail.com>
2014-03-15 14:53:36 -0400

    wip

--- src/cljs/rhyme_finder/rhymer.cljs
+++ src/cljs/rhyme_finder/rhymer.cljs
@@ -2,10 +2,14 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]]
                    [dommy.macros :refer [node sel sel1]])
   (:require [goog.events :as events]
+            [clojure.string :as str]
             [cljs.core.async :refer [<! >! put! take! chan]]
             [cljs.reader :refer [read-string]]
             [dommy.core :as dom]
-            [ajax.core :refer [GET POST]]))
+            [ajax.core :refer [GET POST]])
+   ;         [om.core :as om :include-macros true]
+  ;          [om.dom :as d :include-macros true])
+  )
 
 (enable-console-print!)
 
@@ -54,6 +58,23 @@
   (mapv #(dom/remove-class! % :active) (sel [:.nav :li]))
   (dom/add-class! btn :active))
 
+(defn combo-list [analysis]
+  (node [:ul {:class "nav nav-pills "}
+         (for [combo analysis]
+           [:li
+            [:a {:href "#" :id (str "combo-" (str/join "-" (:value combo)))}
+             [:span {:class "badge pull-right"} (count (first (:streams combo)))]
+             (str/join " " (:value combo))]])]))
+
+(def app-state
+  (atom
+    {:analysis '({:value '("ay" "ow"),
+                 :streams '('([{:phone "ay", :index 5, :word "i"}
+                               {:phone "ow", :index 6, :word "hope"}]
+                              [{:phone "ay", :index 11, :word "i"}
+                               {:phone "ow", :index 12, :word "don't"}]))}),
+     :text "I'm writing a poem today"}))
+
 (defn init []
   ;; for handling new poem additions
   (let [new-analyses (listen (sel1 :#add-poem-form) "submit" true)]
@@ -66,7 +87,10 @@
   (let [get-analysis (listen (sel1 :#get-analysis-form) "submit" true)]
     (go (while true
           (<! get-analysis)
-          (print (<! (handle-get-analysis))))))
+          (let [analysis (<! (handle-get-analysis))]
+            (print analysis)
+            (dom/replace-contents! (sel1 :#combo-list)
+                                   (combo-list (:analysis analysis)))))))
 
   ;; for handling navigation
   (let [view-btn (sel1 :#view-analyses-btn)
@@ -78,4 +102,22 @@
            viewing (handle-nav view-btn (sel1 :#view-analyses))
            analyzing (handle-nav analyze-btn (sel1 :#add-poem)))))))
 
+;(om/root app-state (fn [app owner] (d/h2 nil "test")) (sel1 :combo-list))
+
+;; (om/root
+;;    app-state
+;;    (fn [app owner]
+;;      (apply d/ul #js {:className "nav nav-pills"}
+;;        (map (fn [combo]
+;;               (d/li nil
+;;                 (d/a #js {:href "#"
+;;                           :id (str "combo-" (str/join "-" (:value combo)))}
+;;                   (d/span #js {:className "badge pull-right"}
+;;                           (count (first (:streams combo)))))
+;;                     (str/join " " (:value combo))))
+;;             (:analysis app-state))))
+;;    (sel1 :#combo-list))
+
 (init)
+
+(print "west")
