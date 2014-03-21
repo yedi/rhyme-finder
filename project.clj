4176b15a3cef9598fbd31c49f115bfe57e592796
yedi <yedispaghetti@gmail.com>
2014-03-20 22:43:49 -0400

    add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rhyme-finder "0.1.0-SNAPSHOT"
   :description "Rhyme Analysis Library"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/yedi/rhyme-finder"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
