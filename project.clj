a6325553c8d9e6a6665ffece3889ccd8823d88d2
yedi <yedispaghetti@gmail.com>
2014-03-29 15:14:09 -0400

    cljx

--- project.clj
+++ project.clj
@@ -3,5 +3,14 @@
   :url "https://github.com/yedi/rhyme-finder"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :source-paths ["src/clj"])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths ["src/cljx"]
+
+  :plugins [[com.keminglabs/cljx "0.3.2"]]
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :clj}
+
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :cljs}]})
